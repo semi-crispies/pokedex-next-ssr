50e21a5439bb50ce83ba891ee37615e8d322f7aa
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-04-05 01:29:31 +0200

    WIP use fetch to get pokemon info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Button from "@/components/Button";
+import FetchPokeApi from "@/components/FetchPokeApi";
 
 export default function Home() {
+
     return (
         <main className="">
 
+            <FetchPokeApi/>
+
             <h1 className="flex items-center justify-center py-8">
-                <Image src="/pokelogo.svg" alt="Pokemon Logo" width={500} height={24} priority/>
+                <Image src="/pokelogo.svg" alt="Pokemon Logo" width={500} height={200} priority/>
             </h1>
 
             <Button label={"Jaki"}/>
