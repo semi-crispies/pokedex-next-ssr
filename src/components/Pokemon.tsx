d9ee242d62515bad73752834e7b03cba557d8c21
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-04-08 20:16:01 +0200

    :art: update CSS

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -15,20 +15,19 @@ export default async function Pokemon({id}: ID) {
     const pokemon = await getPokemon(id);
 
     return (
-        <article className="flex-col justify-center w-72">
+        <article className="flex flex-col items-center w-72">
             <Image src={pokemon.getSprite().dream}
-                   width={200}
-                   height={200}
+                   width={200} height={200}
                    alt={`Picture of ${pokemon.getName()}`}/>
 
-            <h1>#{pokemon.getId()} - {capitalizeFirstLetter(pokemon.getName())}</h1>
+            <h1 className="w-full">#{pokemon.getId()} - {capitalizeFirstLetter(pokemon.getName())}</h1>
 
-            <div className="flex flex-row-reverse gap-2">
+            <div className="w-full flex flex-row-reverse gap-2">
                 <span>{pokemon.getHeightMeter()} m</span>
                 <span>{pokemon.getWeightKg()} kg</span>
             </div>
 
-            <ul className="flex justify-center p-4">
+            <ul className="flex py-4 gap-4">
                 {(pokemon.getPokemonType()).map((type: string, index: React.Key) => {
                     return <li key={index}>
                         <Type text={capitalizeFirstLetter(type)} classList={`w-24 ${type}`}/>
