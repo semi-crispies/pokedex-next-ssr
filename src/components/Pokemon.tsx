0a179beaa587b0995d87f59a8c176001affdd412
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-04-08 19:29:19 +0200

    remove useless code

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -3,55 +3,11 @@
 import Image from 'next/image'
 import {getPokemon} from "@/services/fetch";
 import React from "react";
-import {applyTypeColor2, capitalizeFirstLetter} from "@/services/utils";
+import {capitalizeFirstLetter} from "@/services/utils";
 import Type from "@/components/Type";
 
 type ID = {
     id: string,
-}
-
-function applyTypeColor1(type: string): string {
-
-    switch (type) {
-        case 'normal':
-            return 'bg-normal-primary';
-        case 'fighting':
-            return 'bg-fighting-primary';
-        case 'flying':
-            return 'bg-flying-primary';
-        case 'poison':
-            return 'bg-poison-primary';
-        case 'ground':
-            return 'bg-ground-primary';
-        case 'rock':
-            return 'bg-rock-primary';
-        case 'bug':
-            return 'bg-bug-primary';
-        case 'ghost':
-            return 'bg-[#735797]';
-        case 'steel':
-            return 'bg-steel-primary';
-        case 'fire':
-            return 'bg-fire-primary';
-        case 'water':
-            return 'bg-water-primary';
-        case 'grass':
-            return 'bg-grass-primary';
-        case 'electric':
-            return 'bg-electric-primary';
-        case 'psychic':
-            return 'bg-psychic-primary';
-        case 'ice':
-            return 'bg-ice-primary';
-        case 'dragon':
-            return 'bg-dragon-primary';
-        case 'dark':
-            return 'bg-dark-primary'
-        case 'fairy':
-            return 'bg-fairy-primary';
-        default:
-            return "";
-    }
 }
 
 export default async function Pokemon({id}: ID) {
@@ -73,7 +29,7 @@ export default async function Pokemon({id}: ID) {
             <ul>
                 {(pokemon.getPokemonType()).map((type: string, index: React.Key) => {
                     return <li key={index}>
-                        <Type text={capitalizeFirstLetter(type)} classList={applyTypeColor2(type)}/>
+                        <Type text={capitalizeFirstLetter(type)} classList={type}/>
                     </li>
                 })}
             </ul>
