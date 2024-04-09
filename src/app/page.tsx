c2972fcc0ca46dbd25d2857eecf9b940c02dab4a
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-04-09 12:09:37 +0200

    update page.tsx into client component to allow setState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import Image from "next/image";
 import Button from "@/components/Button";
-import Pokemon from "@/components/Pokemon";
+//import Pokemon from "@/components/Pokemon";
+import {useState} from "react";
+import {NextIndex, PreviousIndex} from "@/services/utils";
 
 export default function Home() {
+
+    const [index, setIndex] = useState(1);
 
     return (
         <main className="flex flex-col items-center justify-center py-8">
@@ -11,11 +17,15 @@ export default function Home() {
                 <Image src="/pokelogo.svg" alt="Pokemon Logo" width={500} height={200} priority/>
             </h1>
 
-            <Pokemon id="94"/>
+            {/*<Pokemon id="94"/>*/}
+
+            <div>INDEX {index}</div>
 
             <div className="flex space-x-24">
-                <Button label={"Previous"} classList="bg-gray-300 hover:bg-gray-600 hover:text-white"/>
-                <Button label={"Next"} classList="bg-gray-300 hover:bg-gray-600 hover:text-white"/>
+                <Button label={"Previous"} classList="bg-gray-300 hover:bg-gray-600 hover:text-white"
+                        onClick={() => PreviousIndex(index, setIndex)}/>
+                <Button label={"Next"} classList="bg-gray-300 hover:bg-gray-600 hover:text-white"
+                        onClick={() => NextIndex(index, setIndex)}/>
             </div>
 
         </main>
--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,3 +1,19 @@
 export function capitalizeFirstLetter(string: string): string {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function PreviousIndex(index: number, setIndex: any): void {
+
+    if (index > 1) {
+        setIndex(index - 1);
+    }
+
+}
+
+export function NextIndex(index: number, setIndex: any): void {
+
+    if (index < 151) {
+        setIndex(index + 1);
+    }
+
+}
