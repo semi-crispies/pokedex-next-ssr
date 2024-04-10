7d59715e2379c54ae6a0818ece87952dbfd21eef
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-04-10 16:27:41 +0200

    remove useless function && update previous/nextNumber fct

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,18 +1,33 @@
-export function capitalizeFirstLetter(string: string): string {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+import React from "react";
 
-export function PreviousNumber(number: number, setNumber: any): void {
+/**
+ * Function to decrement number with a minimum value
+ * to avoid negative value
+ *
+ * @param number
+ * @param setNumber
+ * @param min
+ * @constructor
+ */
+export function previousNumber(number: number, setNumber: React.Dispatch<React.SetStateAction<number>>, min: number): void {
 
-    if (number > 1) {
+    if (number > min) {
         setNumber(number - 1);
     }
 
 }
 
-export function NextNumber(number: number, setNumber: any): void {
+/**
+ * Function to increment number with a maximum value
+ *
+ * @param number
+ * @param setNumber
+ * @param max
+ * @constructor
+ */
+export function nextNumber(number: number, setNumber: React.Dispatch<React.SetStateAction<number>>, max: number): void {
 
-    if (number < 151) {
+    if (number < max) {
         setNumber(number + 1);
     }
 
