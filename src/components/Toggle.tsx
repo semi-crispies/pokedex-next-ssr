a320e3e37204d59b3ec6df1e83e675af85b75089
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-04-13 14:15:58 +0200

    fix react warning

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -15,7 +15,7 @@ export default function Toggle({iconName, label, classList, onClick, checked}: t
 
     return (
         <label className={`inline-flex items-center cursor-pointer ${classList}`}>
-            <input type="checkbox" value="" className="sr-only peer" onClick={onClick} checked={checked}/>
+            <input type="checkbox" value="" className="sr-only peer" onClick={onClick} defaultChecked={checked}/>
             <div
                 className="mr-2 flex-shrink-0 relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             {icon}
