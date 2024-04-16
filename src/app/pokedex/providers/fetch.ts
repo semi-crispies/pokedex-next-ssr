a04c9e154050753582fbb9b512d0bf532dfba60c
Witkowski Antoine <witkowski.antoine@gmail.com>
2024-04-16 19:11:28 +0200

    minor update (type)

--- src/app/pokedex/providers/fetch.ts
+++ src/app/pokedex/providers/fetch.ts
@@ -68,7 +68,7 @@ function formatTypes(rawTypes: PokeapiTypes[]): string[] {
     return cleanTypes;
 }
 
-async function getPokemonEvolutionFromAPI(pokemonID: any) {
+async function getPokemonEvolutionFromAPI(pokemonID: any): Promise<PokemonEvolution> {
 
     const evolution: PokemonEvolution = {
         base: {
